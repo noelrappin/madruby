16cbd415a5bb8dc4bb5e4e64b4d00a9e0a387925
Noel Rappin <noel@groupon.com>
2013-07-12 15:32:32 -0500

    initial creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Madruby::Application.config.secret_key_base = 'd71e94b9cc779560571833663fb3e7ea302ed5bcf458ed27f9fc02f2de5b1a6b2f20fd051baf5bdd3e2a52d6abc4bb660767b4b26672130978a847e7e262190d'
